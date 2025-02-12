3c1309a184e27310d70c17e17c872713da8a389c
giandiport80 <giannurwana19@gmail.com>
2025-02-12 19:26:30 +0700

    membuat model category, product, slider

---
+++ app/src/main/java/com/giandiport80/topediaapp/core/data/source/model/Product.kt
@@ -0,0 +1,13 @@
+package com.giandiport80.topediaapp.core.data.source.model
+
+data class Product(
+    val id: String? = null,
+    val name: String?,
+    val harga: Int?,
+    val pengiriman: String?,
+    val terjual: Int?,
+    val rating: Double?,
+    val discount: Int?,
+    val grosir: Boolean = false,
+    val image: Int?
+)
