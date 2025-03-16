ce30ac8ba7a2543b0f6323e653107ec47a55ee8f
giandiport80 <giannurwana19@gmail.com>
2025-03-16 10:41:25 +0700

    setup design detail product

---
+++ app/src/main/java/com/giandiport80/topediaapp/ui/product/DetailProductActivity.kt
@@ -0,0 +1,27 @@
+package com.giandiport80.topediaapp.ui.product
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.giandiport80.topediaapp.R
+import com.giandiport80.topediaapp.databinding.ActivityDetailProductBinding
+
+class DetailProductActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailProductBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailProductBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        mainButton()
+    }
+
+    private fun mainButton() {
+
+    }
+}
